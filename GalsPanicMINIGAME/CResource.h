ffc6b550b03d9ba7a72bbeca1b8600a8ce658784
djmaxjoaa <cpsn6237@gmail.com>
2023-07-26 17:47:38 +0900

    asdfasdf

---
+++ GalsPanicMINIGAME/CResource.h
@@ -0,0 +1,19 @@
+#pragma once
+class CResource
+{
+private:
+	wstring strKey;			// 리소스 키
+	wstring strRelativePath;	// 리소스 경로
+
+public:
+	void SetKey(const wstring& _strKey) { strKey = _strKey; }
+	void SetRelativePath(const wstring& _strPath) { strRelativePath = _strPath; }
+
+	const wstring& GetKey() { return strKey; }
+	const wstring& GetRelativePath() { return strRelativePath; }
+
+public:
+	CResource();
+	~CResource();
+};
+
---
+++ GalsPanicMINIGAME/CTexture.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "CResource.h"
+class CTexture :
+    public CResource
+{
+private:
+    HDC m_dc;
+};
+
